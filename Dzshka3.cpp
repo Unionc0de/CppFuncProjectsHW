9f6eddb31122b2a8bb4e7b17ec5dbbd5e4a11500
Unionc0de <144795080+Unionc0de@users.noreply.github.com>
2023-10-02 19:57:27 +0300

    Add files via upload

---
+++ Dzshka3.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus"); 
+
+    /*char a;*/
+    //cout << "Введите символ\n";//#1 почему то на любой символ отвечает цифра. Делал по таблице asci
+    //cin >> a;
+
+    //if (a >= 48 || a <= 57) {
+    //    cout << "Цифра";
+    //}
+
+    //else if (a == 33 || a == 39 || a == 34 || a == 44 || a == 45 || a == 46 || a == 63) {
+    //    cout << "Знак препинания";
+    //}
+
+    //else if ((a >= 97 || a <= 122) || (a >= 65 || a <= 90)) {
+    //    cout << "Буква";
+    //}
+
+    //else {
+    //    cout << "Другой символ";
+    //}
+
+
+    //int minu,prc = 0, prc2 = 0;
+    //string oper, oper2;
+    //
+
+    //cout << "Введите 1 оператора\n";
+    //cin >> oper;
+    //cout << "Введите 2 оператора\n";
+    //cin >> oper2;
+    //cout << "Введите количество минут\n";
+    //cin >> minu;
+    //
+    //if (oper == oper2) {
+    //    cout << "Звонки на одинаковые операторы бесплатные\n";
+    //}
+
+    //else if (oper == "МТС") {
+    //    prc = 2;
+
+    //}
+    //
+    //else if (oper2 == "МТС") {
+    //    prc2 = 2;
+    //}
+
+    //else if (oper == "Билайн") {
+    //    prc = 1;
+
+    //}
+
+    //else if (oper2 == "Билайн") {
+    //    prc2 = 1;
+    //}
+
+    //else if (oper == "Теле2") {
+    //    prc = 3;
+
+    //}
+
+    //else if (oper2 == "Теле2") {
+    //    prc2 = 3;
+    //}
+
+
+
+    //cout << float((prc + prc2)*minu);
+
+    int opoz, code, income,prib1=0,prib2=0;
+
+    cout << "Введите количество строк кода";
+    cin >> code;
+    cout << "Ввидите количество опозданий";
+    cin >> opoz;
+    cout << "Введите желаемый доход";
+    cin >> income;
+
+
+
+
+
+
+
+
+
+}
+
