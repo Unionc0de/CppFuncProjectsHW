d3e2f78347e3891ca37c3c355186be632f8916d6
Unionc0de <144795080+Unionc0de@users.noreply.github.com>
2023-10-30 22:25:26 +0300

    Проверка количества простых чисел

---
+++ Dz6.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int min, max,j = 0;
+    cout << "Введите диапозоны";
+    cin >> min >> max;
+    if (min > max) swap(min, max);
+
+    for (int i = min; i <= max; i++) {
+        if (i % i == 0 && i % 1 == 0 && (i % 2 <= 1 && i % 2 >= 2)) i++;
+        j = i;
+    }
+    
+    cout << j;//почему то не работает проверка 
+
+}
+
