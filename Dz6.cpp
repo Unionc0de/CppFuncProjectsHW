96ff18a6606c1b689156e55d10e6eb155c234dea
Unionc0de <144795080+Unionc0de@users.noreply.github.com>
2023-11-07 12:14:03 +0300

    Dz6.cpp
    Simple numbers

--- Dz6.cpp
+++ Dz6.cpp
@@ -1,21 +1,17 @@
-﻿
-
-#include <iostream>
-using namespace std;
-int main()
-{
-    setlocale(LC_ALL, "rus");
-    int min, max,j = 0;
-    cout << "Введите диапозоны";
-    cin >> min >> max;
-    if (min > max) swap(min, max);
-
+﻿int min, max;
+    int c = 0;
+    cin >> min;
+    cin >> max;
+    bool f;
     for (int i = min; i <= max; i++) {
-        if (i % i == 0 && i % 1 == 0 && (i % 2 <= 1 && i % 2 >= 2)) j++;
-        
+        f = true;
+        for (int j = 2; j < sqrt(i); j++) {
+            if (i % j == 0) {
+                f = false;
+                break;
+            }
+        }
+        if (f)c++;
     }
-    
-    cout << j;//почему то не работает проверка 
-
-}
+    cout << c;
 
