52305e9da563aeeb396e016a08f922b1f1f083d9
Unionc0de <144795080+Unionc0de@users.noreply.github.com>
2023-11-29 19:48:19 +0300

    Counting sort

---
+++ Dzshka 8.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    srand(time(NULL));
+    const int size = 10;
+    int arr[size];
+
+    // Заполнение массива случайными числами от 1 до 10
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 10 + 1;
+    }
+
+    cout << "Исходный массив: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    
+    int count[size] = { 0 };
+
+    // Подсчет количества каждого элемента в массиве
+    for (int i = 0; i < size; i++) {
+        count[arr[i] - 1]++;
+    }
+
+    // Перезапись отсортированных элементов обратно в массив
+    int index = 0;
+    for (int i = 0; i < size; i++) {
+        while (count[i] > 0) {
+            arr[index++] = i + 1;
+            count[i]--;
+        }
+    }
+
+    std::cout << "\nОтсортированный массив: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+
+
